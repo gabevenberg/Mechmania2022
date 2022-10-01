76fbb1acb842b0f4db7e9897ecabd3e4d2d54d83
Mitchell-Borders <mitchell.borders28@gmail.com>
2022-10-01 10:09:58 -0500

    Smart attack

--- src/strategy/hunting_knight.py
+++ src/strategy/hunting_knight.py
@@ -20,7 +20,7 @@ class HuntingKnight(Strategy):
         self.start_positions = [(0,0), (9, 0), (0, 9), (9, 9)]
 
     def strategy_initialize(self, my_player_index: int):
-        return game.character_class.CharacterClass.ARCHER
+        return game.character_class.CharacterClass.KNIGHT
 
     def move_action_decision(self, game_state: GameState, my_player_index: int) -> Position:
         curr_pos = (game_state.player_state_list[my_player_index].position.x,  game_state.player_state_list[my_player_index].position.y)
@@ -40,7 +40,32 @@ class HuntingKnight(Strategy):
     def attack_action_decision(self, game_state: GameState, my_player_index: int) -> int:
         list = [0,1,2,3]
         list.remove(my_player_index)
+        our_pos = (game_state.player_state_list[my_player_index].position.x,  game_state.player_state_list[my_player_index].position.y)
+        lowest_health_enemy = Random().randint(0,2)
+        lowest_knight_enemy = Random().randint(0,2)
+        hunting_scope_enemy = Random().randint(0,2)
         for enemy in list:
+            enemy_pos = (game_state.player_state_list[enemy].position.x, game_state.player_state_list[enemy].position.y)
+            # get Lowest health enemy in range
+            if game_state.player_state_list[enemy].health < game_state.player_state_list[lowest_health_enemy].health and self.enemy_in_attack_range(our_pos, enemy_pos):
+                lowest_health_enemy = enemy
+            # get Lowest health knight in range
+            if game_state.player_state_list[enemy].health < game_state.player_state_list[lowest_knight_enemy].health and game_state.player_state_list[enemy].character_class == game.character_class.CharacterClass.KNIGHT and self.enemy_in_attack_range(our_pos, enemy_pos):
+                lowest_knight_enemy = enemy
+            # get enemy using hunter scope
+            if game_state.player_state_list[enemy].item == Item.HUNTER_SCOPE and self.enemy_in_attack_range(enemy_pos, our_pos):
+                hunting_scope_enemy = enemy
+            
+            # Free point if they are  a wizard/archer and we are a knight. No matter what we one shot and it's free points
+            if game_state.player_state_list[enemy].character_class == game.character_class.CharacterClass.ARCHER or game_state.player_state_list[enemy].character_class == game.character_class.CharacterClass.WIZARD and self.enemy_in_attack_range(enemy_pos, our_pos):
+                return enemy
+
+            # To determine who to attack, change the index positions of 'final_attack_pos' and the return statement
+            final_attack_pos = (game_state.player_state_list[lowest_health_enemy].position.x, game_state.player_state_list[lowest_health_enemy].position.y)
+            if self.enemy_in_attack_range(final_attack_pos, our_pos):
+                return lowest_health_enemy
+
+            # Random enemy in range if specified 
             if abs(game_state.player_state_list[my_player_index].position.x - game_state.player_state_list[enemy].position.x) <2 and abs(game_state.player_state_list[my_player_index].position.y - game_state.player_state_list[enemy].position.y) <2:
                 return enemy
                 
@@ -62,6 +87,10 @@ class HuntingKnight(Strategy):
                 return True
         return False
 
+    def enemy_in_attack_range(self, enemy_pos, our_pos):
+        if abs(enemy_pos[0] - our_pos[0]) < 2 and abs(enemy_pos[1] - our_pos[1]) < 2:
+            return True
+        return False
 
 
     def get_start_pos(self, curr_pos):
@@ -78,19 +107,19 @@ class HuntingKnight(Strategy):
     def go_to_middle(self, curr_pos):
         if self.start_pos == StartPosEnum.top_left:
             if curr_pos[0] < 4:
-                return (curr_pos[0] + 2, curr_pos[1] + 2)
+                return (curr_pos[0] + 1, curr_pos[1] + 1)
 
         if self.start_pos == StartPosEnum.top_right:
-            if curr_pos[0] > 6:
-                return (curr_pos[0] - 2, curr_pos[1] + 2)
+            if curr_pos[0] > 5:
+                return (curr_pos[0] - 1, curr_pos[1] + 1)
 
         if self.start_pos == StartPosEnum.bottom_left:
             if curr_pos[0] < 4:
-                return (curr_pos[0] + 2, curr_pos[1] - 2)
+                return (curr_pos[0] + 1, curr_pos[1] - 1)
 
         if self.start_pos == StartPosEnum.bottom_right:
-            if curr_pos[0] > 6:
-                return (curr_pos[0] - 2, curr_pos[1] - 2)
+            if curr_pos[0] > 5:
+                return (curr_pos[0] - 1, curr_pos[1] - 1)
 
         return curr_pos
 
